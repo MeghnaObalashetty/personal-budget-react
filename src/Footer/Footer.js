7353311639990081f4db6950af7aaa0b0a5d812c
MeghnaObalashetty <meghnadayanand96@gmail.com>
2023-09-29 14:14:04 -0400

    Brought PBcodein

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return ( 
+        <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Meghna Obalashetty
+        </div>
+    </footer>
+    );
+}
+
+export default Footer;
